8407d2805f48299139057392477b9c561a3da874
Flavio Jr <flavioribeirojr@outlook.com>
2018-09-17 13:49:48 -0300

    Hook fix

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './App.scss'
 
-const App = () => <h1>Teste do site pessoal</h1>
+const App = () => <h1>Teste do site pessoal!</h1>
 
 if (module.hot) {
   module.hot.accept()
